f49d7a1830402dc99d33b560fbea2240fbcdc8b5
unknown <kirchoff200@gmail.com>
2023-02-22 19:28:00 +0100

    melody

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './global.css'
+import LandingPage from './components/LandingPage'
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
